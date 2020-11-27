f8d2d71ee6346576e378565fd716b04603bff4e8
zhj01 <Le#2020!ge>
2020-11-27 09:31:42 +0800

    修改依赖库

--- app/src/main/java/com/lege/commonview/FirstActivity.kt
+++ app/src/main/java/com/lege/commonview/FirstActivity.kt
@@ -1,14 +1,29 @@
 package com.lege.commonview
 
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val mActionBar = supportActionBar
+        mActionBar?.hide()
+        val decorView = window.decorView
+        var systemUiVisibility = decorView.systemUiVisibility
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                //                 | View.SYSTEM_UI_FLAG_FULLSCREEN       // hide status bar
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        systemUiVisibility = systemUiVisibility or flags
+        decorView.systemUiVisibility = systemUiVisibility
+        window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_first)
-        startActivity(Intent(this,TikTokActivity::class.java))
+//        startActivity(Intent(this,TikTokActivity::class.java))
     }
 }
